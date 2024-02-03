51af2128db5849765e8d61ff02d9f1bb621ebfa6
spacewalkingninja <spacewalkingninja@gmail.com>
2024-02-03 00:50:53 +0000

    sahdjkashdjkshdasjk gfix

--- executables/Windows/automatic.service.bat
+++ executables/Windows/automatic.service.bat
@@ -12,5 +12,5 @@ type nul > %model_path%\NSSM.flag
 call %model_path%\env\python -m pip install -r %model_path%\requirements.txt
 :mainloop
 :: Start Model Server
-call %model_path%\env\python %model_path%\webui.bat --docs --lowvram 
+call %model_path%\webui.bat --docs --lowvram 
 GOTO mainloop
